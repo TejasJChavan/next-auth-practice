e3d7a5912c4c77ec593092f51bd92d00f60cc0ca
Tejas Chavan <64842361+TejasJChavan@users.noreply.github.com>
2025-07-07 21:18:23 +0530

    Bcrypt error fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "./db";
-import { compare } from "bcrypt";
+import { compare } from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
